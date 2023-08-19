7edb76f16a3161eddd21f86dd2698664e24d6fec
txuyuan <191519R@student.hci.edu.sg>
2023-08-20 00:22:50 +0800

    feat(index): animate in by section

--- pages/index.vue
+++ pages/index.vue
@@ -21,74 +21,82 @@
     <div class="masthead-space"></div>
 
     <main>
-      <h1>
-        Science and Technology for Humanity: Building a Sustainable Future
-      </h1>
-      <p>
-        ISYF 2024 endeavours to inspire delegates to use Science and Technology
-        to lead the change into the future by reconnecting, reimagining, and
-        looking ahead, to create a better world marked by common goals, shared
-        knowledge and global solidarity.
-      </p>
-      <p>
-        The International Science Youth Forum (ISYF) is a prestigious science
-        enrichment programme for top research students from all over the world,
-        hosted by Hwa Chong Institution’s Student Science Research Club (SSRC).
-        The delegates of ISYF will have the opportunity to participate in a
-        multitude of exciting and meaningful activities, which include
-        intellectual discourse with Nobel laureates and eminent scientists who
-        utilise technology and data to make decisions. Delegates will also be
-        able to network with peers through collaborative projects and cultural
-        exchanges.
-      </p>
-      <p>We hope to see you at ISYF!</p>
+      <section>
+        <h1>
+          Science and Technology for Humanity: Building a Sustainable Future
+        </h1>
+        <p>
+          ISYF 2024 endeavours to inspire delegates to use Science and
+          Technology to lead the change into the future by reconnecting,
+          reimagining, and looking ahead, to create a better world marked by
+          common goals, shared knowledge and global solidarity.
+        </p>
+        <p>
+          The International Science Youth Forum (ISYF) is a prestigious science
+          enrichment programme for top research students from all over the
+          world, hosted by Hwa Chong Institution’s Student Science Research Club
+          (SSRC). The delegates of ISYF will have the opportunity to participate
+          in a multitude of exciting and meaningful activities, which include
+          intellectual discourse with Nobel laureates and eminent scientists who
+          utilise technology and data to make decisions. Delegates will also be
+          able to network with peers through collaborative projects and cultural
+          exchanges.
+        </p>
+        <p>We hope to see you at ISYF!</p>
+      </section>
 
-      <h1>Events</h1>
-      <p>
-        At ISYF 2024, you can look forward to Amazing Race, Lorem ipsum dolor
-        sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-        ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat
-      </p>
+      <section>
+        <h1>Events</h1>
+        <p>
+          At ISYF 2024, you can look forward to Amazing Race, Lorem ipsum dolor
+          sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+          incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+          commodo consequat
+        </p>
 
-      <Button href="#" icon="east">See Our Full Programme!</Button>
-      <Button href="#">Our Invited Speakers</Button>
+        <Button href="#" icon="east">See Our Full Programme!</Button>
+        <Button href="#">Our Invited Speakers</Button>
+      </section>
 
-      <h1>How can you join?</h1>
+      <section>
+        <h1>How can you join?</h1>
+        <p>
+          Participation in ISYF is via invitation to selected schools across the
+          globe.
+        </p>
+        <p>
+          The programme targets top science students from schools all over the
+          world. Student participants from each school should meet the following
+          criteria:
+        </p>
+        <ul>
+          <li>Be between 16 and 17 years of age in 2023,</li>
+          <li>Be within the top tier of their respective Science cohorts,</li>
+          <li>
+            Have substantial experience in science research or competitions,
+          </li>
+          <li>Possess good oral proficiency in English, and</li>
+          <li>
+            Possess enthusiasm and strong interest in forging ties with fellow
+            students from all over the world.
+          </li>
+        </ul>
+      </section>
 
-      <p>
-        Participation in ISYF is via invitation to selected schools across the
-        globe.
-      </p>
-      <p>
-        The programme targets top science students from schools all over the
-        world. Student participants from each school should meet the following
-        criteria:
-      </p>
-      <ul>
-        <li>Be between 16 and 17 years of age in 2023,</li>
-        <li>Be within the top tier of their respective Science cohorts,</li>
-        <li>
-          Have substantial experience in science research or competitions,
-        </li>
-        <li>Possess good oral proficiency in English, and</li>
-        <li>
-          Possess enthusiasm and strong interest in forging ties with fellow
-          students from all over the world.
-        </li>
-      </ul>
-
-      <h1>Lorem Ipsum Dolor Sit Amet. Header</h1>
-      <p>
-        ISYF is hosted and organised by Hwa Chong Institution’s very own Student
-        Science Research Club (SSRC). Lorem ipsum dolor sit amet, consectetur
-        adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-        magna aliqua. Ut enim ad minim veniam
-      </p>
-      <p>
-        We are also deeply honoured to have the support and help of these
-        organisations:
-      </p>
+      <section>
+        <h1>Lorem Ipsum Dolor Sit Amet. Header</h1>
+        <p>
+          ISYF is hosted and organised by Hwa Chong Institution’s very own
+          Student Science Research Club (SSRC). Lorem ipsum dolor sit amet,
+          consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+          labore et dolore magna aliqua. Ut enim ad minim veniam
+        </p>
+        <p>
+          We are also deeply honoured to have the support and help of these
+          organisations:
+        </p>
+      </section>
     </main>
   </div>
 </template>
@@ -197,7 +205,9 @@
 import { onMounted } from "vue";
 import Masthead from "../components/masthead.vue";
 import Button from "../components/button/button.vue";
+
 import { gsap } from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
 
 onMounted(() => {
   gsap.from(".masthead .info *", {
@@ -215,5 +225,19 @@ onMounted(() => {
     delay: 1,
     ease: "back.inOut",
   });
+
+  gsap.registerPlugin(ScrollTrigger)
+
+  const sections = gsap.utils.toArray('main section');
+
+  sections.forEach((section) => {
+    gsap.from(section, {
+      opacity: 0,
+      y: 10,
+      duration: 0.5,
+      ease: "power2.inOut",
+      scrollTrigger: section
+    })
+  })
 });
 </script>
